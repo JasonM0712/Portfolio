b8b11f30e534e87c59a23664ee5d3dc1cbe31608
JasonM0712 <155554008+JasonM0712@users.noreply.github.com>
2026-03-23 21:14:26 +0100

    Modal fonctionnel ajustement sur la page accueil 23/03/2026

--- asset/script.js
+++ asset/script.js
@@ -9,3 +9,35 @@ const navbar = document.querySelector(".navigation");
     }
 
 });
+
+// Ouvrir la modal
+document.querySelectorAll("button[data-modal]").forEach(button => {
+  button.addEventListener("click", function() {
+    const modalId = this.getAttribute("data-modal");
+    document.getElementById(modalId).style.display = "block";
+  });
+});
+
+// Fermer la modal avec le X
+document.querySelectorAll(".close").forEach(closeBtn => {
+  closeBtn.addEventListener("click", function() {
+    this.closest(".modal").style.display = "none";
+  });
+});
+
+// Fermer en cliquant en dehors
+window.addEventListener("click", function(e) {
+  document.querySelectorAll(".modal").forEach(modal => {
+    if (e.target === modal) {
+      modal.style.display = "none";
+    }
+  });
+});
+
+const burger = document.querySelector(".burger");
+const menuMobile = document.querySelector(".navButtonMobile");
+
+burger.addEventListener("click", () => {
+    menuMobile.classList.toggle("OpenBurger");
+    menuMobile.classList.toggle("closeBurger");
+});
